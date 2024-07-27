952747244eeb745d9e033ac8bc1f80e7ca31dd70
Sachin Prabhu Ram <sachin.prabhu.ram12@gmail.com>
2024-07-27 18:16:54 +0800

    Uploaded words.py to get a list of valid words. Updated function to check the list as well

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from words import word_dict
+
 GUESSES = 6 # Hard-coded. Should not change
 count = 1
 Word_of_the_day = 'AWASH'
@@ -8,7 +10,7 @@ while count <= GUESSES:
     guess = input("\nEnter your guess here: ").upper()
     guess_list = list(guess)
     
-    if len(guess_list)== 5:
+    if guess in word_dict:
         if guess == Word_of_the_day: 
             print(f"You got the word right in {count} guesses")
             break
@@ -24,7 +26,7 @@ while count <= GUESSES:
             
             count += 1
     else:
-        print("Enter a 5 letter word")
+        print("Enter a valid word")
 
     
 
