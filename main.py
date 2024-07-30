624da3692c2673f94fe58b80645a8b9e02839f3a
Sachin Prabhu Ram <sachin.prabhu.ram12@gmail.com>
2024-07-30 11:58:01 +0800

    Refactored code

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from words import word_dict
 import random
 
+def generate_word():
+    generated_word = random.choice(word_dict)
+    return generated_word
+
+def show_guess(guess_list, word_list):
+    for i in range(0,len(word_list)):
+        if guess_list[i] == word_list[i]:
+            print(f"The letter {word_list[i]} is in the main word at the position {i+1}")
+        elif guess_list[i] in word_list:
+            print(f"The letter {guess_list[i]} is in the main word at a different position")
+        else:
+            print(f"The letter {guess_list[i]} is not in the main word")
+
 GUESSES = 6 # Hard-coded. Should not change
 count = 1
-Word_of_the_day = random.choice(word_dict)
-print(Word_of_the_day)
+Word_of_the_day = generate_word()
 
 while count <= GUESSES:
     word_list = list(Word_of_the_day)
@@ -18,14 +30,7 @@ while count <= GUESSES:
             break
         else:
             print('\n')
-            for i in range(0,len(word_list)):
-                if guess_list[i] == word_list[i]:
-                    print(f"The letter {word_list[i]} is in the main word at the position {i+1}")
-                elif guess_list[i] in word_list:
-                    print(f"The letter {guess_list[i]} is in the main word at a different position")
-                else:
-                    print(f"The letter {guess_list[i]} is not in the main word")
-            
+            show_guess(guess_list, word_list)
             count += 1
     else:
         print("Enter a valid word")
