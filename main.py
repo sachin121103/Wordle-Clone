98a4bcbac643e5ab0fc2a117c5a192ce262ea97e
Sachin Prabhu Ram <sachin.prabhu.ram12@gmail.com>
2024-07-31 11:56:21 +0800

    Created a function to make the game more user-friendly

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from words import word_dict
 import random
 
 def generate_word():
+
+    """Generates the word randomly from words.py"""
+
     generated_word = random.choice(word_dict)
     return generated_word
 
 def show_guess(guess_list, word_list):
+
+    """The role of this function is to process the guess and compare it with the original word"""
+
     for i in range(0,len(word_list)):
         if guess_list[i] == word_list[i]:
             print(f"The letter {word_list[i]} is in the main word at the position {i+1}")
@@ -14,9 +20,15 @@ def show_guess(guess_list, word_list):
         else:
             print(f"The letter {guess_list[i]} is not in the main word")
 
+def intro_game():
+    """This function introduces the game itself and how it is to be played."""
+    print("This game is a clone of Wordle.\nTo play, guess any valid 5 letter word. Follow the clues to get the correct word.\nYou get only 6 guesses")
+
 GUESSES = 6 # Hard-coded. Should not change
 count = 1
 Word_of_the_day = generate_word()
+
+intro_game()
 
 while count <= GUESSES:
     word_list = list(Word_of_the_day)
@@ -26,7 +38,7 @@ while count <= GUESSES:
     
     if guess in word_dict:
         if guess == Word_of_the_day: 
-            print(f"You got the word right in {count} guesses")
+            print(f"Congrats!You got the word right in {count} guesses!")
             break
         else:
             print('\n')
